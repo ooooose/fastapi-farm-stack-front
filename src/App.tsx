38b99eb8f358364c15de6f5f0abd7d942865bfc3
ooooose <yuuki.oose1990@gmail.com>
2023-09-14 07:22:32 +0900

    csrfトークンの取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { CsrfToken } from './types/types';
+import { useAppSelector } from './app/hooks';
+import { selectCsrfState } from './slices/appSlice';
 
 function App() {
+  const csrf = useAppSelector(selectCsrfState);
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const res = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrftoken`
+      )
+      axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrf_token
+    }
+    getCsrfToken()
+  }, [csrf])
   return (
-    <div className="App">
+    <div>
     </div>
   );
 }
